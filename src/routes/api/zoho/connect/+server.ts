5808b6c66116be5a80e54b4c47967a9550bcc5c2
NerdyGymDude <111654134+NerdyGymDude@users.noreply.github.com>
2026-08-24 15:53:09 +0000

    Add Zoho inbox read scope

--- src/routes/api/zoho/connect/+server.ts
+++ src/routes/api/zoho/connect/+server.ts
@@ -36,7 +36,7 @@ export const GET: RequestHandler = async ({ url, cookies }) => {
 
 	authorizationUrl.searchParams.set(
 		'scope',
-		'ZohoMail.messages.CREATE,ZohoMail.accounts.READ'
+		'ZohoMail.messages.CREATE,ZohoMail.messages.READ,ZohoMail.accounts.READ'
 	);
 
 	authorizationUrl.searchParams.set(
